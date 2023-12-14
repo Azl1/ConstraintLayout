9ae34c8c986106dcbc80dd60c533ec7d46a5ee5b
Aziz <abdullaev12@bk.ru>
2023-12-14 18:51:06 +0300

    InVisible

--- app/src/main/java/com/abdullaev/constraintlayout/MainActivity.java
+++ app/src/main/java/com/abdullaev/constraintlayout/MainActivity.java
@@ -3,12 +3,58 @@ package com.abdullaev.constraintlayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button visible, inVisible;
+
+    Button mc, mr, ms, mPlus, mMinus;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        view();
+        visible.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mc.setVisibility(View.VISIBLE);
+                mr.setVisibility(View.VISIBLE);
+                ms.setVisibility(View.VISIBLE);
+                mPlus.setVisibility(View.VISIBLE);
+                mMinus.setVisibility(View.VISIBLE);
+            }
+        });
+
+        inVisible.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mc.setVisibility(View.INVISIBLE);
+                mr.setVisibility(View.INVISIBLE);
+                ms.setVisibility(View.INVISIBLE);
+                mPlus.setVisibility(View.INVISIBLE);
+                mMinus.setVisibility(View.INVISIBLE);
+            }
+        });
+
+
+
     }
+
+    public void view() {
+        visible = findViewById(R.id.buttonVisible);
+        inVisible = findViewById(R.id.buttonInVisible);
+
+        mc = findViewById(R.id.buttonMC);
+        mr = findViewById(R.id.buttonMR);
+        ms = findViewById(R.id.buttonMS);
+        mPlus = findViewById(R.id.buttonMPlus);
+        mMinus = findViewById(R.id.buttonMMinus);
+    }
+
 }
