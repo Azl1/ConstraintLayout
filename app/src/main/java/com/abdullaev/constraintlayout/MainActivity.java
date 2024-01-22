83ec45e04f7f509da0044779e7275077acd1fb93
Aziz <abdullaev12@bk.ru>
2024-01-23 01:18:27 +0300

    InVisible

--- app/src/main/java/com/abdullaev/constraintlayout/MainActivity.java
+++ app/src/main/java/com/abdullaev/constraintlayout/MainActivity.java
@@ -19,7 +19,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setContentView(R.layout.layoutland);
         view();
         visible.setOnClickListener(new View.OnClickListener() {
             @Override
